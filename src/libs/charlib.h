c85db034ce9a8a1065853a9e6c76de2fda484b97
UnexomWid <mihaitinu@gmail.com>
2020-01-05 15:36:47 +0200

    Modified charlib guards name.

--- src/libs/charlib.h
+++ src/libs/charlib.h
@@ -1,30 +1,30 @@
-#ifndef CHARLIB_H
-#define CHARLIB_H
+#ifndef CHARLIB_H_INCLUDED
+#define CHARLIB_H_INCLUDED
 
 #include <string>
 
-std::string toString(const char token);
+std::string toString(char token);
 
 std::string toLower(const std::string &token);
 
-bool isSpace(const char token);
+bool isSpace(char token);
 
-bool isDigit(const char token);
+bool isDigit(char token);
 
-bool isAlpha(const char token);
+bool isAlpha(char token);
 
-bool isLeftSeparator(const char token);
+bool isLeftSeparator(char token);
 
-bool isRightSeparator(const char token);
+bool isRightSeparator(char token);
 
 bool isSeparator(char token);
 
-bool isDecimalSeparator(const char token);
+bool isDecimalSeparator(char token);
 
-bool isArgumentSeparator(const char token);
+bool isArgumentSeparator(char token);
 
 bool isNullOrWhitespace(const std::string &token);
 
 bool isNumber(const std::string &token);
 
-#endif // CHARLIB_H
+#endif // CHARLIB_H_INCLUDED
